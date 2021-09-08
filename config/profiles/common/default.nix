91d6ad3d034c21fd40b512c14721899b6ed10910
Iris System <iris@iris.ac.nz>
2021-09-09 11:15:56 +1200

    add tailscale

--- config/profiles/common/default.nix
+++ config/profiles/common/default.nix
@@ -11,5 +11,6 @@
     ./access.nix
     ./packages.nix
     ./machine-cert.nix
+    ./tailscale.nix
   ];
 }
---
+++ config/profiles/common/tailscale.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = [ pkgs.tailscale ];
+  services.tailscale.enable = true;
+
+  networking.firewall = {
+    trustedInterfaces = [ "tailscale0" ];
+    allowedUDPPorts = [ config.services.tailscale.port ];
+  };
+}
