18a7f2515d94808ef14143b1e7f01fbe0dadeea8
Iris System <iris@iris.ac.nz>
2025-03-11 10:39:42 +1300

    remove WSLg packages

--- packages/default.nix
+++ packages/default.nix
@@ -7,6 +7,4 @@ rec {
   u1f408-x = pkgs.callPackage ./u1f408-x {};
   opencore-drivers = pkgs.callPackage ./opencore/drivers.nix {};
 
-  freerdp-wslg = pkgs.callPackage ./freerdp-wslg {};
-  weston-wslg = pkgs.callPackage ./weston-wslg { inherit freerdp-wslg; };
 }
